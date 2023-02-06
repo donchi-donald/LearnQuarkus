e2a0a0877b3ca48b11d21a28a418a5016d695110
donchi-donald <donchidonald2000@gmail.com>
2023-02-06 22:57:39 +0100

    exposing the book rest endpoint

--- src/main/java/org/agoncal/quarkus/starting/BookResource.java
+++ src/main/java/org/agoncal/quarkus/starting/BookResource.java
@@ -2,21 +2,22 @@ package org.agoncal.quarkus.starting;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.awt.print.Book;
 import java.util.List;
+import java.util.Optional;
 
 @Path("/api/books")
 @Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
     @GET
-    public List<Book> hello() {
+    public List<Book> getAllBooks() {
         return List.of(
-                new Book(),
-                new Book(),
-                new Book()
+                new Book(1, "Amour de vie", "Donald Donchi", 2022, "Romantique"),
+                new Book(2, "Tuerie", "Donald Donchi", 2023, "Action"),
+                new Book(3, "GP Valser", "Donald Donchi", 2022, "Assasin")
         );
     }
 
@@ -24,7 +25,15 @@ public class BookResource {
     @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
     public int countAllBooks(){
-        return 20;
-        //return getAllBooks().size();
+        return getAllBooks().size();
     }
+
+    @GET
+    @Path("{id}")
+    public Optional<Book> getBook(@PathParam("id") int id){
+        return getAllBooks().stream().filter(
+                book -> book.id == id).findFirst();
+    }
+
+
 }
--- src/test/java/org/agoncal/quarkus/starting/Book.java
+++
@@ -1,9 +0,0 @@
-package org.agoncal.quarkus.starting;
-
-public class Book {
-    public int id;
-    public String title;
-    public String author;
-    public int yearOfPublication;
-    public String genre;
-}
